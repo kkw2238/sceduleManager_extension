b1cb157aaa093bea5426f344635944dd37542324
kkw2238 <kkw2238@naver.com>
2024-08-27 15:21:26 +0900

    init: 프로그램 생성

---
+++ src/main/java/com/sparta/scedulemanager_extension/SceduleManagerExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.scedulemanager_extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SceduleManagerExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SceduleManagerExtensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/scedulemanager_extension/SceduleManagerExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.scedulemanager_extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SceduleManagerExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
